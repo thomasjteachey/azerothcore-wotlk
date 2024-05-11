3f8d9045f68b4aa1064008e050d3b741ab898ec7
tjteachey <thomasjteachey@gmail.com>
2024-05-11 18:45:14 -0400

    adding thrall's socks to all instances of honor gain

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -6197,6 +6197,8 @@ void Player::SetArenaPoints(uint32 value)
 
 void Player::ModifyHonorPoints(int32 value, CharacterDatabaseTransaction trans)
 {
+    //add thrall's socks
+    AddItem(40752, value);
     int32 newValue = int32(GetHonorPoints()) + value;
     if (newValue < 0)
         newValue = 0;
